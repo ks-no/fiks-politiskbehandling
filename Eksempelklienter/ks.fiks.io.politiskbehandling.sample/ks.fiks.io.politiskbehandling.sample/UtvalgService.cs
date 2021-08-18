07ed423a91b3c3f38df2519915f03249c6238aa2
jarle børsheim <jarle.borsheim@gmail.com>
2021-08-18 14:42:10 +0200

    Endret til å bruke konstanter for typer fra nuget-pakken

--- Eksempelklienter/ks.fiks.io.politiskbehandling.sample/ks.fiks.io.politiskbehandling.sample/UtvalgService.cs
+++ Eksempelklienter/ks.fiks.io.politiskbehandling.sample/ks.fiks.io.politiskbehandling.sample/UtvalgService.cs
@@ -1,13 +1,3 @@
-using Ks.Fiks.Maskinporten.Client;
-using KS.Fiks.ASiC_E;
-using KS.Fiks.IO.Client;
-using KS.Fiks.IO.Client.Configuration;
-using KS.Fiks.IO.Client.Models;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Schema;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -15,6 +5,16 @@ using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
+using KS.Fiks.ASiC_E;
+using KS.Fiks.IO.Client;
+using KS.Fiks.IO.Client.Configuration;
+using KS.Fiks.IO.Client.Models;
+using KS.Fiks.IO.Client.Models.Feilmelding;
+using KS.Fiks.IO.Politiskbehandling.Client.Models;
+using Ks.Fiks.Maskinporten.Client;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
 using RabbitMQ.Client;
 
 namespace ks.fiks.io.politiskbehandling.sample
@@ -39,7 +39,7 @@ namespace ks.fiks.io.politiskbehandling.sample
 
             client.NewSubscription(OnReceivedMelding);
 
-            Console.WriteLine("Abonnerer på meldinger på konto " + appSettings.FiksIOConfig.FiksIoAccountId + " ...");
+            Console.WriteLine($"Abonnerer på meldinger på konto {appSettings.FiksIOConfig.FiksIoAccountId} ...");
 
             return Task.CompletedTask;
         }
@@ -110,37 +110,37 @@ namespace ks.fiks.io.politiskbehandling.sample
             //Se oversikt over meldingstyper på https://github.com/ks-no/fiks-io-meldingstype-katalog/tree/test/schema
 
             // Process the message
-            if (mottatt.Melding.MeldingType == "no.ks.fiks.politisk.behandling.klient.hentutvalg.v1")
+            if (mottatt.Melding.MeldingType == PolitiskBehandlingMeldingTypeV1.HentUtvalg)
             {
-                Console.WriteLine("Melding " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType + " mottas...");
+                Console.WriteLine($"Melding med meldingid {mottatt.Melding.MeldingId} og type {mottatt.Melding.MeldingType} mottas...");
 
-                string payload = File.ReadAllText("sampleResultatUtvalg.json");
+                var payload = File.ReadAllText("sampleResultatUtvalg.json");
 
                 var errorMessages = ValidateJsonFile(payload, Path.Combine("schema", "no.ks.fiks.politisk.behandling.resultatutvalg.v1.schema.json"));
 
                 if (errorMessages[0].Count == 0)
                 {
-                    var svarmsg = mottatt.SvarSender.Svar("no.ks.fiks.politisk.behandling.tjener.resultatutvalg.v1", payload, "resultat.json").Result;
-                    Console.WriteLine("Svarmelding " + svarmsg.MeldingId + " " + svarmsg.MeldingType + " sendt...");
+                    var svarmsg = mottatt.SvarSender.Svar(PolitiskBehandlingMeldingTypeV1.ResultatUtvalg, payload, "resultat.json").Result;
+                    Console.WriteLine($"Svarmelding på meldingid {svarmsg.MeldingId} med type {svarmsg.MeldingType} sendt...");
                     Console.WriteLine(payload);
                     mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                 }
                 else
                 {
                     Console.WriteLine("Feil i validering av utvalg");
-                    mottatt.SvarSender.Svar("no.ks.fiks.kvittering.ugyldigforespørsel.v1", String.Join("\n ", errorMessages[0]), "feil.txt");
-                    Console.WriteLine(String.Join("\n ", errorMessages[0]));
+                    mottatt.SvarSender.Svar(FeilmeldingMeldingTypeV1.Ugyldigforespørsel, string.Join("\n ", errorMessages[0]), "feil.txt");
+                    Console.WriteLine(string.Join("\n ", errorMessages[0]));
                     mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                 }
 
             }
-            else if (mottatt.Melding.MeldingType == "no.ks.fiks.politisk.behandling.klient.hentmøteplan.v1")
+            else if (mottatt.Melding.MeldingType == PolitiskBehandlingMeldingTypeV1.HentMøteplan)
             {
-                Console.WriteLine("Melding " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType + " mottas...");
+                Console.WriteLine($"Melding med meldingid {mottatt.Melding.MeldingId} og type {mottatt.Melding.MeldingType} mottas...");
 
                 if (mottatt.Melding.HasPayload)
-                { // Verify that message has payload
-                    List<List<string>> errorMessages = new List<List<string>>() { new List<string>(), new List<string>() };
+                { 
+                    var errorMessages = new List<List<string>>() { new List<string>(), new List<string>() };
                     IAsicReader reader = new AsiceReader();
                     using (var inputStream = mottatt.Melding.DecryptedStream.Result)
                     using (var asice = reader.Read(inputStream))
@@ -154,56 +154,55 @@ namespace ks.fiks.io.politiskbehandling.sample
                                     errorMessages = ValidateJsonFile(new StreamReader(entryStream).ReadToEnd(), Path.Combine("schema", "no.ks.fiks.politisk.behandling.hentmøteplan.v1.schema.json"));
                                 }
                                 else
-                                    Console.WriteLine("Mottatt vedlegg: " + asiceReadEntry.FileName);
+                                    Console.WriteLine($"Mottatt vedlegg: {asiceReadEntry.FileName}");
                             }
                         }
                     }
 
                     if (errorMessages[0].Count == 0)
                     {
-                        string payload = File.ReadAllText("sampleResultat.json");
+                        var payload = File.ReadAllText("sampleResultat.json");
 
                         errorMessages = ValidateJsonFile(payload, Path.Combine("schema", "no.ks.fiks.politisk.behandling.resultatmøteplan.v1.schema.json"));
 
                         if (errorMessages[0].Count == 0)
                         {
-                            var svarmsg = mottatt.SvarSender.Svar("no.ks.fiks.politisk.behandling.tjener.resultatmøteplan.v1", payload, "resultat.json").Result;
-                            Console.WriteLine("Svarmelding " + svarmsg.MeldingId + " " + svarmsg.MeldingType + " sendt...");
+                            var svarmsg = mottatt.SvarSender.Svar(PolitiskBehandlingMeldingTypeV1.ResultatMøteplan, payload, "resultat.json").Result;
+                            Console.WriteLine($"Svarmelding på meldingid {svarmsg.MeldingId} for type {svarmsg.MeldingType} sendt...");
                             Console.WriteLine(payload);
                             mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                         }
                         else
                         {
                             Console.WriteLine("Feil i validering av resultatmøteplan");
-                            mottatt.SvarSender.Svar("no.ks.fiks.kvittering.ugyldigforespørsel.v1", String.Join("\n ", errorMessages[0]), "feil.txt");
-                            Console.WriteLine(String.Join("\n ", errorMessages[0]));
+                            mottatt.SvarSender.Svar(FeilmeldingMeldingTypeV1.Ugyldigforespørsel, string.Join("\n ", errorMessages[0]), "feil.txt");
+                            Console.WriteLine(string.Join("\n ", errorMessages[0]));
                             mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                         }
                     }
                     else
                     {
                         Console.WriteLine("Feil i validering av hentmøteplan");
-                        mottatt.SvarSender.Svar("no.ks.fiks.kvittering.ugyldigforespørsel.v1", String.Join("\n ", errorMessages[0]), "feil.txt");
-                        Console.WriteLine(String.Join("\n ", errorMessages[0]));
+                        mottatt.SvarSender.Svar(FeilmeldingMeldingTypeV1.Ugyldigforespørsel, string.Join("\n ", errorMessages[0]), "feil.txt");
+                        Console.WriteLine(string.Join("\n ", errorMessages[0]));
                         mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                     }
                 }
                 else
                 {
-                    var svarmsg = mottatt.SvarSender.Svar("no.ks.fiks.kvittering.ugyldigforespørsel.v1", "Meldingen mangler innhold", "feil.txt").Result;
+                    var svarmsg = mottatt.SvarSender.Svar(FeilmeldingMeldingTypeV1.Ugyldigforespørsel, "Meldingen mangler innhold", "feil.txt").Result;
                     Console.WriteLine("Svarmelding " + svarmsg.MeldingId + " " + svarmsg.MeldingType + " Meldingen mangler innhold");
 
                     mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
-
                 }
             }
-            else if (mottatt.Melding.MeldingType == "no.ks.fiks.politisk.behandling.klient.sendutvalgssak.v1")
+            else if (mottatt.Melding.MeldingType == PolitiskBehandlingMeldingTypeV1.SendUtvalgssak)
             {
-                Console.WriteLine("Melding " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType + " mottas...");
+                Console.WriteLine($"Melding med meldingid {mottatt.Melding.MeldingId} type {mottatt.Melding.MeldingType} mottas...");
 
                 if (mottatt.Melding.HasPayload)
                 {
-                    List<List<string>> errorMessages = new List<List<string>>() { new List<string>(), new List<string>() };
+                    var errorMessages = new List<List<string>>() { new List<string>(), new List<string>() };
                     IAsicReader reader = new AsiceReader();
                     using (var inputStream = mottatt.Melding.DecryptedStream.Result)
                     using (var asice = reader.Read(inputStream))
@@ -217,7 +216,7 @@ namespace ks.fiks.io.politiskbehandling.sample
                                     errorMessages = ValidateJsonFile(new StreamReader(entryStream).ReadToEnd(), Path.Combine("schema", "no.ks.fiks.politisk.behandling.sendutvalgssak.v1.schema.json"));
                                 }
                                 else
-                                    Console.WriteLine("Mottatt vedlegg: " + asiceReadEntry.FileName);
+                                    Console.WriteLine($"Mottatt vedlegg: {asiceReadEntry.FileName}");
                             }
                         }
                     }
@@ -231,27 +230,26 @@ namespace ks.fiks.io.politiskbehandling.sample
                     else
                     {
                         Console.WriteLine("Feil i validering av sendutvalgsak");
-                        mottatt.SvarSender.Svar("no.ks.fiks.kvittering.ugyldigforespørsel.v1", String.Join("\n ", errorMessages[0]), "feil.txt");
-                        Console.WriteLine(String.Join("\n ", errorMessages[0]));
+                        mottatt.SvarSender.Svar(FeilmeldingMeldingTypeV1.Ugyldigforespørsel, string.Join("\n ", errorMessages[0]), "feil.txt");
+                        Console.WriteLine(string.Join("\n ", errorMessages[0]));
                         mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                     }
                 }
                 else
                 {
-                    var svarmsg = mottatt.SvarSender.Svar("no.ks.fiks.kvittering.ugyldigforespørsel.v1", "Meldingen mangler innhold", "feil.txt").Result;
+                    var svarmsg = mottatt.SvarSender.Svar(FeilmeldingMeldingTypeV1.Ugyldigforespørsel, "Meldingen mangler innhold", "feil.txt").Result;
                     Console.WriteLine("Svarmelding " + svarmsg.MeldingId + " " + svarmsg.MeldingType + " Meldingen mangler innhold");
-
                     mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
 
                 }
             }
-            else if (mottatt.Melding.MeldingType == "no.ks.fiks.politisk.behandling.klient.sendorienteringssak.v1")
+            else if (mottatt.Melding.MeldingType == PolitiskBehandlingMeldingTypeV1.SendOrienteringssak)
             {
                 Console.WriteLine("Melding " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType + " mottas...");
 
                 if (mottatt.Melding.HasPayload)
                 {
-                    List<List<string>> errorMessages = new List<List<string>>() { new List<string>(), new List<string>() };
+                    var errorMessages = new List<List<string>>() { new List<string>(), new List<string>() };
                     IAsicReader reader = new AsiceReader();
                     using (var inputStream = mottatt.Melding.DecryptedStream.Result)
                     using (var asice = reader.Read(inputStream))
@@ -279,42 +277,33 @@ namespace ks.fiks.io.politiskbehandling.sample
                     else
                     {
                         Console.WriteLine("Feil i validering av sendorienteringssak");
-                        mottatt.SvarSender.Svar("no.ks.fiks.kvittering.ugyldigforespørsel.v1", String.Join("\n ", errorMessages[0]), "feil.txt");
-                        Console.WriteLine(String.Join("\n ", errorMessages[0]));
+                        mottatt.SvarSender.Svar(FeilmeldingMeldingTypeV1.Ugyldigforespørsel, string.Join("\n ", errorMessages[0]), "feil.txt");
+                        Console.WriteLine(string.Join("\n ", errorMessages[0]));
                         mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
                     }
                 }
                 else
                 {
-                    var svarmsg = mottatt.SvarSender.Svar("no.ks.fiks.kvittering.ugyldigforespørsel.v1", "Meldingen mangler innhold", "feil.txt").Result;
+                    var svarmsg = mottatt.SvarSender.Svar(FeilmeldingMeldingTypeV1.Ugyldigforespørsel, "Meldingen mangler innhold", "feil.txt").Result;
                     Console.WriteLine("Svarmelding " + svarmsg.MeldingId + " " + svarmsg.MeldingType + " Meldingen mangler innhold");
-
                     mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
-
                 }
             }
-            else if (mottatt.Melding.MeldingType == "no.ks.fiks.politisk.behandling.klient.senddelegertvedtak.v1")
+            else if (mottatt.Melding.MeldingType == PolitiskBehandlingMeldingTypeV1.SendDelegertVedtak)
             {
                 Console.WriteLine("Melding " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType + " mottas...");
 
                 //TODO håndtere meldingen med ønsket funksjonalitet
-
                 var svarmsg = mottatt.SvarSender.Svar("no.ks.fiks.politisk.behandling.mottatt.v1").Result;
                 Console.WriteLine("Svarmelding " + svarmsg.MeldingId + " " + svarmsg.MeldingType + " sendt...");
-
-
                 mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
             }
             else if (mottatt.Melding.MeldingType == "no.ks.fiks.politisk.behandling.mottatt.v1")
             {
                 Console.WriteLine("Melding " + mottatt.Melding.MeldingId + " " + mottatt.Melding.MeldingType + " mottas...");
-
                 //TODO håndtere meldingen med ønsket funksjonalitet
-
                 Console.WriteLine("Melding er håndtert i ePlansak ok ......");
-
                 mottatt.SvarSender.Ack(); // Ack message to remove it from the queue
-
             }
             else
             {
@@ -325,7 +314,7 @@ namespace ks.fiks.io.politiskbehandling.sample
 
         private static List<List<string>> ValidateJsonFile(string jsonString, string pathToSchema)
         {
-            List<List<string>> validationErrorMessages = new List<List<string>>() { new List<string>(), new List<string>() };
+            var validationErrorMessages = new List<List<string>>() { new List<string>(), new List<string>() };
             using (TextReader file = File.OpenText(pathToSchema))
             {
                 JObject jObject = JObject.Parse(jsonString);
