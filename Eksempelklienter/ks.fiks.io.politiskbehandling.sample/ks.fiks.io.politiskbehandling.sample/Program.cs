371255ada304d04a0a847123a7f807ec7107a14d
Jarle Børsheim <jarle.borsheim@gmail.com>
2021-10-20 14:54:42 +0200

    Lagt til Serilog, refaktorert for bedre lesbarhet og endret returtyper der det var nødvendig med siste update (#19)

--- Eksempelklienter/ks.fiks.io.politiskbehandling.sample/ks.fiks.io.politiskbehandling.sample/Program.cs
+++ Eksempelklienter/ks.fiks.io.politiskbehandling.sample/ks.fiks.io.politiskbehandling.sample/Program.cs
@@ -1,14 +1,11 @@
-﻿using Ks.Fiks.Maskinporten.Client;
-using KS.Fiks.IO.Client;
-using KS.Fiks.IO.Client.Configuration;
-using KS.Fiks.IO.Client.Models;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
 
 namespace ks.fiks.io.politiskbehandling.sample
 {
@@ -16,6 +13,33 @@ namespace ks.fiks.io.politiskbehandling.sample
     {
         static async Task Main(string[] args)
         {
+            var aspnetcoreEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var logstashDestination = Environment.GetEnvironmentVariable("LOGSTASH_DESTINATION");
+            var hostname = Environment.GetEnvironmentVariable("HOSTNAME");
+            var kubernetesNode = Environment.GetEnvironmentVariable("KUBERNETES_NODE");
+            var environment = Environment.GetEnvironmentVariable("ENVIRONMENT");
+            
+            var loggerConfiguration = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Localization", LogEventLevel.Error)
+                .Enrich.FromLogContext()
+                .Enrich.WithProperty("app", "politisk-behandling-simulator")
+                .Enrich.WithProperty("env", environment)
+                .Enrich.WithProperty("logsource", hostname)
+                .Enrich.WithProperty("node", kubernetesNode)
+                .WriteTo.Console(outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}] [{Level}] [{RequestId}] [{requestid}] - {Message} {NewLine} {Exception}");
+
+            Log.Logger = loggerConfiguration.CreateLogger();
+
+            Log.Information("Starting host with env variables:");
+            Log.Information("ASPNETCORE_ENVIRONMENT: {AspnetcoreEnvironment}", aspnetcoreEnvironment);
+            Log.Information("HOSTNAME: {Hostname}", hostname);
+            Log.Information("KUBERNETES_NODE: {KubernetesNode}", kubernetesNode);
+            Log.Information("ENVIRONMENT: {Environment}",environment);
+            Log.Information("LOGSTASH_DESTINATION: {LogstashDestination}", logstashDestination);
+            Log.Information("Path.PathSeparator: {PathSeparator}", Path.PathSeparator);
+            
             await new HostBuilder()
                 .ConfigureHostConfiguration((configHost) =>
                 {
@@ -32,9 +56,8 @@ namespace ks.fiks.io.politiskbehandling.sample
                 {
                     services.AddSingleton(AppSettingsBuilder.CreateAppSettings(hostContext.Configuration));
                     services.AddHostedService<UtvalgService>();
-                })
+                }).UseSerilog()
                 .RunConsoleAsync();
         }
-
     }
 }
